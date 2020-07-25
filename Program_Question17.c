6c5cf88f4554f3cf97598869d87eaccfacc03ed0
Gabriel Robinson <gabrielrobinson000@gmail.com>
2020-07-25 07:59:23 -0500

    Even number printer programs

---
+++ Program_Question17.c
@@ -0,0 +1,10 @@
+/* This program prints the even numbers from 4 to 20 and there squares*/
+#include <stdio.h>
+int main(){
+	/* this will use the easy solution that somewhat works*/
+	int x;
+	for(x = 4; x <= 20; x = x + 2){
+	printf(" %d",x);
+	}
+return 0;
+}
---
+++ Program_Question17_V2.c
@@ -0,0 +1,13 @@
+/* This program prints the even numbers from 4 to 20 and there squares Version 2*/
+#include <stdio.h>
+int main(){
+        
+        int x;
+        for(x = 4; x <= 20; x++){
+		if(x % 2 == 0){
+			printf(" %d",x);
+		}
+        }
+return 0;
+}
+
